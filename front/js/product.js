5e202f978e50d6e5a2916bba3713b74b2172520a
Matthieu Thepaut <matth.thepautpgmail.com>
2021-10-26 18:21:45 +0200

    correction script

--- front/js/product.js
+++ front/js/product.js
@@ -1,18 +1,23 @@
-console.log(window.location.search);
-const itemContainer = document.querySelector(".item");
-const id = window.location.search.slice(4);
 const apiUrl = "http://localhost:3000/api/products/";
+const getById = function (id) {
+  return document.getElementById(id);
+};
+const createEl = function (tag) {
+  return document.createElement(tag);
+};
+
 const cart = JSON.parse(sessionStorage.getItem("cart")) || [];
 console.log(cart);
-const generateColors = function (colors) {
-  return colors.reduce(
-    (acc, color) => acc + `<option value="${color}">${color}</option>`,
-    ``
-  );
+const generateColor = function (color) {
+  const option = createEl("option");
+  option.setAttribute("value", color);
+  option.textContent = color;
+  return option;
 };
 const getProduct = async function (url) {
   try {
     const response = await fetch(url);
+    if (!response.ok) throw new Error("Un problème est survenu!");
     const data = await response.json();
     return data;
   } catch (err) {
@@ -20,86 +25,132 @@ const getProduct = async function (url) {
   }
 };
 const renderProduct = async function (url) {
-  const product = await getProduct(url);
+  const id = new URLSearchParams(document.location.search).get("id");
+  const product = await getProduct(url + id);
+  const itemContainer = document.querySelector(".item");
   console.log(product);
-  console.log(product.colors);
-  const colors = generateColors(product.colors);
-  console.log(colors);
 
-  const markup = `
-<article>
-    <div class="item__img">
-       <img src="${product.imageUrl}" alt="${product.altTxt}"> 
-    </div>
-    <div class="item__content">
-         <div class="item__content__titlePrice">
-          <h1 id="title">${product.name}</h1>
-          <p>
-                  Prix : <span id="price">${product.price}</span>€
-          </p>
-         </div>
+  let article = createEl("article");
+  itemContainer.appendChild(article);
 
-         <div class="item__content__description">
-          <p class="item__content__description__title">Description :</p>
-          <p id="description">${product.description}
-          </p>
-         </div>
+  let div1 = createEl("div");
+  div1.setAttribute("class", "item__img");
+  article.appendChild(div1);
 
-         <div class="item__content__settings">
-            <div class="item__content__settings__color">
-                  <label for="color-select">Choisir une couleur :</label>
-                  <select name="color-select" id="colors">
-                    <option value="">--SVP, choisissez une couleur --</option>
-                    ${colors}
-            
-                  </select>
-            </div>
+  let img = createEl("img");
+  img.setAttribute("src", product.imageUrl);
+  img.setAttribute("alt", product.altTxt);
+  div1.appendChild(img);
 
-            <div class="item__content__settings__quantity">
-                  <label for="itemQuantity"
-                    >Nombre d'article(s) (1-100) :</label
-                  >
-                  <input
-                    type="number"
-                    name="itemQuantity"
-                    min="1"
-                    max="100"
-                    value="0"
-                    id="quantity"
-                  />
-            </div>
-         </div>
-         <div class="item__content__addButton">
-                <button id="addToCart">Ajouter au panier</button>
-        </div>
-    </div>
-</article>
+  let div2 = createEl("div");
+  div2.setAttribute("class", "item__content");
+  article.appendChild(div2);
 
-        `;
+  let div3 = createEl("div");
+  div3.setAttribute("class", "item__content__titlePrice");
+  div2.appendChild(div3);
 
-  itemContainer.insertAdjacentHTML("afterbegin", markup);
-  const btn = document.getElementById("addToCart");
-  const quantityContainer = document.getElementById("quantity");
-  const colorContainer = document.getElementById("colors");
-  console.log(quantityContainer.value);
+  let titre = createEl("h1");
+  titre.setAttribute("id", "title");
+  titre.textContent = product.name;
+  div3.appendChild(titre);
 
-  btn.addEventListener("click", function (e) {
-    const prod = { ...product };
-    if (quantityContainer.value < 1 || !colorContainer.value) return;
-    const index = cart.findIndex((prod) => prod._id === id);
-    console.log(index);
+  let para1 = createEl("p");
+  let node1 = document.createTextNode("Prix : ");
+  let node2 = document.createTextNode("€");
+  let span1 = createEl("span");
+  span1.setAttribute("id", "price");
+  span1.textContent = product.price;
+  para1.appendChild(node1);
+  para1.appendChild(span1);
+  para1.appendChild(node2);
+  div3.appendChild(para1);
 
-    if (index > -1 && cart[index].colors == colorContainer.value) {
-      cart[index].quantity += +quantityContainer.value;
-    } else {
-      prod.quantity = +quantityContainer.value;
-      prod.colors = colorContainer.value;
-      cart.push(prod);
-    }
+  let div4 = createEl("div");
+  div4.setAttribute("class", "item__content__description");
+  div2.appendChild(div4);
 
-    console.log(cart);
-    sessionStorage.setItem("cart", JSON.stringify(cart));
+  let para2 = createEl("p");
+  para2.setAttribute("class", "item__content__description__title");
+  para2.textContent = "Descritpion :";
+  div4.appendChild(para2);
+
+  let para3 = createEl("p");
+  para3.setAttribute("id", "description");
+  para3.textContent = product.description;
+  div4.appendChild(para3);
+
+  let div5 = createEl("div");
+  div5.setAttribute("class", "item__content__settings");
+  div2.appendChild(div5);
+
+  let div6 = createEl("div");
+  div6.setAttribute("class", "item__content__settings__color");
+  div5.appendChild(div6);
+
+  let label1 = createEl("label");
+  label1.setAttribute("for", "color-select");
+  label1.textContent = "Choisir une couleur :";
+  div6.appendChild(label1);
+
+  let select = createEl("select");
+  select.setAttribute("name", "color-select");
+  select.setAttribute("id", "colors");
+  div6.appendChild(select);
+
+  let option = createEl("option");
+  option.setAttribute("value", "");
+  option.textContent = "--SVP, choisissez une couleur --";
+  select.appendChild(option);
+
+  const options = product.colors.map((color) => generateColor(color));
+  options.forEach((option) => {
+    select.appendChild(option);
   });
+
+  let div7 = createEl("div");
+  div7.setAttribute("class", "item__content__settings__quantity");
+  div5.appendChild(div7);
+
+  let label2 = createEl("label");
+  label2.setAttribute("for", "itemQuantity");
+  label2.textContent = "Nombre d'article(s) (1-100) :";
+  div7.appendChild(label2);
+
+  let input = createEl("input");
+  input.setAttribute("type", "number");
+  input.setAttribute("name", "itemQuantity");
+  input.setAttribute("min", "1");
+  input.setAttribute("max", "100");
+  input.setAttribute("value", "0");
+  input.setAttribute("id", "quantity");
+  div7.appendChild(input);
+
+  let div8 = createEl("div");
+  div8.setAttribute("class", "item__content__addButton");
+  div2.appendChild(div8);
+
+  let button = createEl("button");
+  button.setAttribute("id", "addToCart");
+  button.textContent = "Ajouter au panier";
+  // button.addEventListener("click", function (e) {
+  //   const prod = { ...product };
+  //   if (quantityContainer.value < 1 || !colorContainer.value) return;
+  //   const index = cart.findIndex((prod) => prod._id === id);
+  //   console.log(index);
+
+  //   if (index > -1 && cart[index].colors == colorContainer.value) {
+  //     cart[index].quantity += +quantityContainer.value;
+  //   } else {
+  //     prod.quantity = +quantityContainer.value;
+  //     prod.colors = colorContainer.value;
+  //     cart.push(prod);
+  //   }
+
+  //   console.log(cart);
+  //   sessionStorage.setItem("cart", JSON.stringify(cart));
+  // });
+  div8.appendChild(button);
 };
 
-renderProduct(apiUrl + id);
+renderProduct(apiUrl);
--- front/js/script.js
+++ front/js/script.js
@@ -1,37 +1,55 @@
 "use strict";
+const apiUrl = "http://localhost:3000/api/products/";
+const getById = function (id) {
+  return document.getElementById(id);
+};
+const createEl = function (tag) {
+  return document.createElement(tag);
+};
 
-const itemsContainer = document.getElementById("items");
-
-const products = [];
 const getProducts = async function (url) {
   try {
     const response = await fetch(url);
+    if (!response.ok) throw new Error("Un problème est survenu");
     const data = await response.json();
-    data.forEach((product) => products.push(product));
-    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+const renderProducts = async function (url) {
+  try {
+    const products = await getProducts(url);
+    console.log(products);
+    const itemsContainer = getById("items");
+    products.forEach((product) => {
+      let link = createEl("a");
+      link.setAttribute("href", `front/product.html?id=${product._id}`);
+
+      let article = createEl("article");
+      link.appendChild(article);
+
+      let img = createEl("img");
+      img.setAttribute("src", product.imageUrl);
+      img.setAttribute("alt", product.altTxt);
+      article.appendChild(img);
+
+      let titre = createEl("h3");
+      titre.setAttribute("class", "productName");
+      titre.textContent = product.name;
+      article.appendChild(titre);
+
+      let para = createEl("p");
+      para.setAttribute("class", "productDescription");
+      para.textContent = product.description;
+      article.appendChild(para);
+
+      itemsContainer.appendChild(link);
+    });
   } catch (err) {
     console.log(err);
   }
 };
 
-const renderProducts = async function (url) {
-  await getProducts(url);
-  console.log(products);
-  products.forEach((product) => {
-    const markup = `
-        <a href="front/product.html?id=${product._id}">
-          <article>
-              <img
-                      src="${product.imageUrl}"
-                      alt=${product.altTxt}
-                     />
-              <h3 class="productName">${product.name}</h3>
-              <p class="productDescription">${product.description}
-              </p>
-          </article>
-        </a>`;
-    itemsContainer.insertAdjacentHTML("afterbegin", markup);
-  });
-};
-
-renderProducts("http://localhost:3000/api/products");
+renderProducts(apiUrl);
